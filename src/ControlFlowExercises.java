19060ee755259c57cb5b2c904a39df28d3892407
adrianperez <adrian.perez.456@gmail.com>
2017-09-20 11:04:57 -0500

    Reviewed in class.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
+
 //        int i = 5;
 //        while (i <= 15) {
 //            System.out.print(i + " ");
@@ -51,6 +52,7 @@ public class ControlFlowExercises {
 //            System.out.println(k);
 //            k=(int) Math.pow(k,2);
 //        }
+
         //FizzBuzz
 //        for(int i=1; i<=100; i++){
 //            if(i%15==0){
@@ -70,72 +72,86 @@ public class ControlFlowExercises {
 
 
         Scanner input= new Scanner(System.in);
-//        System.out.println("What number would you like to go up to? "+ input);
+
+//        while(true) {
+//        System.out.println("What number would you like to go up to? ");
 //        int number=input.nextInt();
+//        input.nextLine();
+//
+//        System.out.println("number | squared | cubed\n" +
+//                "------ | ------- | -----");
 //        for(int i=1; i<=number; i++){
 //            int sq=(int) Math.pow(i,2);
 //            int cub= (int) Math.pow(i,3);
+//            System.out.printf("%-7s| ",i);
+//            System.out.printf("%-8s| ", sq);
+//            System.out.printf("%-5s", cub);
+//            System.out.println("");
+//        }
+//            System.out.println("Do you want to continue? (y/n)");
+//            String option =input.nextLine();
+//            if(!option.equalsIgnoreCase("y")){
+//                System.out.println("Breaking Loop");
+//                break;
+//            }
 //
-//            System.out.println("Number: "+i+" Square: "+sq+" Cube: "+cub);
 //        }
-
         // Grades
-//        System.out.println("Please enter the the grade you received.");
-//        int grade=input.nextInt();
-//        if(100>=grade&&grade>=90){
-//            if(100>=grade&&grade>=93)
-//            System.out.println("A");
-//            else{
-//                System.out.println("A-");
-//            }
-//        }
-//        else if(89>=grade&&grade>=80){
-//            if(89>=grade&&grade>=87) {
-//                System.out.println("B+");
-//            }
-//            else if(86>=grade&&grade>=83) {
-//                System.out.println("B");
-//            }
-//            else{
-//                System.out.println("B-");
-//            }
-//        }
-//        else if (79>=grade&&grade>=70){
-//            if(79>=grade&&grade>=77) {
-//                System.out.println("C+");
-//            }
-//            else if(76>=grade&&grade>=73) {
-//                System.out.println("C");
-//            }
-//            else{
-//                System.out.println("C-");
-//            }
-//        }
-//        else if(69>=grade&&grade>60){
-//            if(69>=grade&&grade>=67) {
-//                System.out.println("D+");
-//            }
-//            else if(66>=grade&&grade>=63) {
-//                System.out.println("D");
-//            }
-//            else{
-//                System.out.println("D-");
-//            }
-//        }
-//        else{
-//            System.out.println("F");
-//        }
+        while(true) {
+            System.out.println("Please enter the the grade you received.");
+            int grade = input.nextInt();
+            input.nextLine();
+            if (100 >= grade && grade >= 90) {
+                if (100 >= grade && grade >= 93)
+                    System.out.println("A");
+                else {
+                    System.out.println("A-");
+                }
+            } else if (89 >= grade && grade >= 80) {
+                if (89 >= grade && grade >= 87) {
+                    System.out.println("B+");
+                } else if (86 >= grade && grade >= 83) {
+                    System.out.println("B");
+                } else {
+                    System.out.println("B-");
+                }
+            } else if (79 >= grade && grade >= 70) {
+                if (79 >= grade && grade >= 77) {
+                    System.out.println("C+");
+                } else if (76 >= grade && grade >= 73) {
+                    System.out.println("C");
+                } else {
+                    System.out.println("C-");
+                }
+            } else if (69 >= grade && grade >= 60) {
+                if (69 >= grade && grade >= 67) {
+                    System.out.println("D+");
+                } else if (66 >= grade && grade >= 63) {
+                    System.out.println("D");
+                } else {
+                    System.out.println("D-");
+                }
+            } else if (grade < 59) {
+                System.out.println("F");
+            }
 
+            System.out.println("Do you want to continue? (y/n)");
+            String option =input.nextLine();
+            if(!option.equalsIgnoreCase("y")){
+                System.out.println("Breaking Loop");
+                break;
+            }
+        }
         //Bonus Pyramid thingy
-        System.out.println("Please enter a number and letter.");
-         int length=input.nextInt();
-         String characterPrint=input.nextLine();
-         for(int i=0; i<length; i++) {
-             for (int j = 0; j<=i; j++){
-                 System.out.print(characterPrint+" ");
-             }
-             System.out.println("");
-         }
+//        System.out.println("Please enter a number and letter.");
+//         int length=input.nextInt();
+//         String characterPrint=input.nextLine();
+//         for(int i=0; i<length; i++) {
+//             for (int j = 0; j<=i; j++){
+//                 System.out.print(characterPrint+" ");
+//             }
+//             System.out.println("");
+//         }
     }
 
 }
