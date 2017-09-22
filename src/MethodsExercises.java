ae2ad4ce52a02867da489228a4f676b7a68e0c12
adrianperez <adrian.perez.456@gmail.com>
2017-09-22 16:07:30 -0500

    Finished the factorial, and modified the dice game to be more dynamic and responsive.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -4,24 +4,28 @@ import java.util.Scanner;
 public class MethodsExercises {
 
     public static void main(String[] args) {
+        Scanner userResponse = new Scanner(System.in);
+
 //        System.out.println(add(5,6));
 //        System.out.println(subtract(5,6));
 //        System.out.println(multiply( 5,8));
 //        System.out.println(divide(5,6));
 //        System.out.println(modulus(8,6));
 //        System.out.println("");
-        factorial();
-//        dice();
+//        getIntegar(1,100);
+//        factorial();
+//        dice(userResponse);
 //        gameDevelopment();
+//        factorial(getIntegar(1,10));
     }
     //1
-    public static int add(int x, int y){
+    public static double add(double x, double y){
         return x+y;
     }
-    public static int subtract(int x, int y){
+    public static double subtract(double x, double y){
         return x-y;
     }
-    public static int multiply(int x, int y){
+    public static double multiply(double x, double y){
         int result=0;
         while(x>0){
             result+=y;
@@ -38,66 +42,62 @@ public class MethodsExercises {
     //It would break the code diving by 0 is infinite.
 
     //2
-    public static void getIntegar(int min, int max) {
-        while(true) {
+    public static int getIntegar(int min, int max) {
+
             System.out.println("Please enter a number between " + min + " and " + max);
             Scanner userInput = new Scanner(System.in);
             int number = userInput.nextInt();
-            if (number >= min && number <= max) {
-                System.out.println("That is a valid input");
-                break;
-            } else {
-                System.out.println("That is not a valid input.");
-            }
-        }
-    }
-    //3
-    public static int factorial(){
-        getIntegar(1,100);
-        System.out.println("Please enter the number you would like to factorial.");
-        Scanner userInput=new Scanner(System.in);
-        int number=userInput.nextInt();
-        int count=1;
-        for(int i=number;i>=1; i--) {
-//            System.out.print(i+"! = "+ (i)+" x "+ " = "+count);
-            count=count*i; // This the factorial output
-            System.out.print(i);
+            if (number < min || number > max) {
+                System.out.println("That is not a valid input");
+                getIntegar(min, max);
 
-            for (int j = number; j >= 1; j--) {
-                System.out.print(j);
+            } else {
+                System.out.println("That is a valid input.");
+                return number;
             }
+            return 0;
+
+
+    }
+
+
+    //3
+    public static void factorial(int number){
+
+        System.out.println("Please enter the number you would like to factorial.");
+        int acum=1;
+        String details="";
+        for(int i=1;i<=number; i++) {
+            details+=(i==1)? i:" x "+i;
+            System.out.println(i+"! = "+details+" = "+(acum*=i));
+            ;
         }
-        return number;
     }
 
     //4
-public static void dice() {
+public static void dice(Scanner input) {
+    System.out.println("Ready to roll? (y/n)");
     while (true) {
-        System.out.println("Ready to roll? (y/n)");
-        Scanner userResponse = new Scanner(System.in);
-        String response = userResponse.nextLine();
+        String response = input.nextLine();
         if (response.equalsIgnoreCase("y")) {
-            Scanner diceNumber = new Scanner(System.in);
             System.out.println("How many dice are you going to roll.");
-            int numberOfDice = diceNumber.nextInt();
-            Scanner userInput = new Scanner(System.in);
+            int numberOfDice = input.nextInt();
             System.out.println("How many sides are on this dice?");
-            int number = userInput.nextInt();
+            int number = input.nextInt();
             Random dice = new Random();
-            Scanner diceRoll = new Scanner(System.in);
             System.out.println("How many times do you want to roll this dice?");
-            int rollCount = diceRoll.nextInt();
-            if (response.equalsIgnoreCase("y")) {
+            int rollCount = input.nextInt();
                 for (int count = 0; count < rollCount; count++) {
+                    System.out.println("Roll "+(count+1));
                     for (int i = 1; i <= numberOfDice; i++) {
                         int randomNumber = dice.nextInt(number);
-                        System.out.println("Random No: " + (randomNumber + 1)); //Random number includes 0 added 1 to start the dice at 1.
+                        System.out.println("Die: "+i+" is " +(randomNumber + 1)); //Random number includes 0 added 1 to start the dice at 1.
                         //This generates the random dice roll.
                     }
                 }
-            }
+            System.out.println("Roll again? (y/n)");
         }
-        if(!response.equalsIgnoreCase("y")) {
+        else if (response.equalsIgnoreCase("n")){
             System.out.println("Alrighty, well bye!");
             break;
         }
