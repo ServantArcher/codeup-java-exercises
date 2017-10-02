fe517c238e4207733ebd19022f16af845a73ae72
adrianperez <adrian.perez.456@gmail.com>
2017-10-02 11:20:27 -0500

    Completed Program: Will continue to run until, user asks to exit.

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -6,13 +6,13 @@ import java.util.HashMap;
 
 public class GradesApplication {
     public static void main(String[] args) {
-
+        //Code that starts the program.
         Input input = new Input();
         startGitHubGrade(input);
-
     }
 
     public static void startGitHubGrade(Input input) {
+        //Created a HashMap that contains all of the users and grades.
         HashMap<String, String> students = new HashMap();
         Student student1 = new Student("Sauron", 90);
         student1.addGrades(87);
@@ -35,18 +35,21 @@ public class GradesApplication {
         students.put(student3.getNames(), "BurdenBarer");
         students.put(student4.getNames(), "gaFFer");
 
+        //Arrays are made in order to index through them latter on.
+        //Names can then be added to later on if needed.
         String[] users = {student1.getNames(), student2.getNames(), student3.getNames(), student4.getNames()};
         Student[] studentClass={student1,student2,student3,student4};
 
+        //Prints the list of users based on their GitHub names.
         System.out.println("");
         System.out.println("Here are the list of gitHub users:");
-
         for (int i = 0; i < users.length; i++) {
             System.out.print(" | ");
             System.out.print(students.get(users[i]));
             System.out.print(" | ");
         }
 
+        //Checks to see that the user entered a valid name from the given list of users.
         String userInput;
         do {
             System.out.println("");
@@ -55,29 +58,35 @@ public class GradesApplication {
             if (!students.containsValue(userInput)) {
                 System.out.println("Sorry " + userInput + " is not a registered user");
             }
+
+            //If an appropriate name is passed through. The program will then begin to get the users information.
             else {
-                getStudentInfo(userInput, students, users,studentClass);
-                break;
+                    getStudentInfo(userInput, students, users, studentClass);
+                    Input moreInfo = new Input();
+                    System.out.println("Would you like to see another student?");
+                    //If the user asks result returns true the info program will run again and ask user for a different username.
+                    if(moreInfo.yesNo()){
+                        getStudentInfo(String.valueOf(moreInfo), students, users, studentClass);
+                    }
+                    else{
+                        break;
+                    }
+                }
             }
-        }
         while (true);
     }
+            //Method will pass various arguments in order to index through the appropriate student's information.
+            public static void getStudentInfo(String userInput, HashMap students, String[] users, Student[] studentClass) {
+                    for (int i = 0; i < users.length; i++) {
+                        //Indexes to the username passed by the user.
+                        if (userInput.equals(students.get(users[i]))) {
+                            System.out.println("Name: " + studentClass[i].getNames() + " - Github UserName: " + users[i]);
+                            System.out.println("The student's " + studentClass[i].getNames() + " average grade is: " + studentClass[i].getGradeAverage());
+                        }
 
-            public static void getStudentInfo(String userInput, HashMap students, String[] users, Student[] studentClass){
-            while(true) {
-                for(int i=0; i< users.length; i++)
-                if (userInput.equals(students.get(users[i]))) {
-                        System.out.println("Name: "+studentClass[i].getNames()+" - Github UserName: "+users[i]);
-                    System.out.println("The student's "+studentClass[i].getNames()+" average grade is: "+studentClass[i].getGradeAverage());
-                    System.out.println("Would you like to see another student?");
-                }
-
-
-                break;
+                    }
                 }
             }
 
 
 
-
-}
